9d1ef11f7985abe0b47e92335b0591fa77d19a23
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2022-08-06 23:42:48 +0530

    Add final project

--- ListAndGrids/app/src/main/java/divyansh/tech/listandgrids/MainActivity.kt
+++ ListAndGrids/app/src/main/java/divyansh/tech/listandgrids/MainActivity.kt
@@ -3,7 +3,19 @@ package divyansh.tech.listandgrids
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ListItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import divyansh.tech.listandgrids.ui.theme.ListAndGridsTheme
 
 private val listOfText = arrayListOf<String>()
@@ -27,7 +40,63 @@ class MainActivity : ComponentActivity() {
             listOfImages.add(R.drawable.dog)
         }
         setContent {
+//            ShowTextList()
+//            ShowLazyTextList()
+            ShowLazyVerticalGrid()
+        }
+    }
+}
 
+@Composable
+fun ShowTextList() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(
+                enabled = true, state = ScrollState(0)
+            )
+            .padding(20.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        listOfText.forEach {
+            Text(text = it)
+        }
+    }
+}
+
+@Composable
+fun ShowLazyTextList() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+//        for (string in listOfText) {
+//            item {
+//                Text(text = string, modifier = Modifier.padding(vertical = 10.dp))
+//            }
+//        }
+        items(listOfText) { item ->
+            Text(text = item)
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ShowLazyVerticalGrid() {
+    LazyVerticalGrid(
+        cells = GridCells.Adaptive(minSize = 100.dp),
+        modifier = Modifier.padding(10.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        items(listOfImages) {item ->
+            Image(
+                painter = painterResource(id = item),
+                contentDescription = ""
+            )
         }
     }
 }
