b94b396b7e67b3f8890ac9d0d5dcf5ec6bd3e729
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2022-08-13 16:38:45 +0530

    Add Ultimate Guide to Animation Project

--- Jetpack-Compose-Basics/app/src/main/java/divyansh/tech/jetpackcomposebasics/MainActivity.kt
+++ Jetpack-Compose-Basics/app/src/main/java/divyansh/tech/jetpackcomposebasics/MainActivity.kt
@@ -1,25 +1,100 @@
 package divyansh.tech.jetpackcomposebasics
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Column(
-                modifier = Modifier.background(Color.Blue).fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp),
+                horizontalAlignment = Alignment.Start
             ) {
-                Text(text = "Something")
-                Text(text = "Something")
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(SpanStyle(
+                            color = Color.Red,
+                            fontSize = 30.sp
+                        )) {
+                            append("L")
+                        }
+                        append("atest")
+                    },
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 24.sp
+                )
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    ImageCard(
+                        image = painterResource(id = R.drawable.dog),
+                        text = "Jetpack Compose"
+                    )
+
+                    Spacer(modifier = Modifier.padding(horizontal = 10.dp))
+
+                    ImageCard(
+                        image = painterResource(id = R.drawable.dog),
+                        text = "Jetpack Compose"
+                    )
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    image: Painter,
+    text: String
+) {
+    Card(
+        modifier = Modifier
+            .height(200.dp)
+            .width(150.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp,
+        backgroundColor = Color.Blue
+    ) {
+        Image(
+            painter = image, contentDescription = text,
+            modifier = Modifier
+                .height(200.dp)
+                .width(150.dp),
+            contentScale = ContentScale.Crop
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            contentAlignment = Alignment.BottomStart,
+        ) {
+            Text(text = text, fontSize = 12.sp, fontWeight = FontWeight.Bold, color = Color.White)
         }
     }
 }
---
+++ UltimateGuideToAnimation/app/src/main/java/divyansh/tech/ultimateguidetoanimation/MainActivity.kt
@@ -0,0 +1,31 @@
+package divyansh.tech.ultimateguidetoanimation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.*
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import divyansh.tech.ultimateguidetoanimation.ui.theme.UltimateGuideToAnimationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
